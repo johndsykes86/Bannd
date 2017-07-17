cb1248141c25fdb60f2a2eee61174907a8100ea2
Connie Duong <connieduong@gmail.com>
2017-07-17 14:07:17 -0700

    created comment model

--- models/Business.js
+++ models/Business.js
@@ -4,8 +4,6 @@ const businessSchema = new mongoose.Schema({
   yelpID: {type: String, required: true}
   name: {type: String, required: true},
   address: {type: String, required: true},
-
 })
 
-
-const User = mongoose.model('Business', businessSchema)
+const User = mongoose.model('Business', businessSchema);
