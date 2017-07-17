561bb94af5705810d3d2f3f2b3920fcc2d18b911
Johnathon Sykes <hireme@mrjsykes.com>
2017-07-17 12:06:27 -0700

    initial comment

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var User = mongoose.Schema({
+  local : {
+    email        : String,
+    password     : String,
+    firstName    : String,
+    lastName     : String,
+  }
+});
+
+User.methods.encrypt = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+User.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', User);
---
+++ server.js
@@ -0,0 +1,38 @@
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var ejsLayouts   = require("express-ejs-layouts");
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+mongoose.connect('mongodb://localhost/Bannd');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.set("views","./views");
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+
+app.use(function (req, res, next){
+  global.user = req.user
+  next()
+})
+
+var routes = require('./config/routes');
+app.use(routes);
+
+app.listen(3000);
