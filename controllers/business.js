ccd1f14e4cc273be24c9b9c6fa2aca288f89d59f
Johnathon Sykes <hireme@mrjsykes.com>
2017-07-18 10:33:08 -0700

    finished createBusiness function in controller/business.js and tested route via Postman

--- controllers/business.js
+++ controllers/business.js
@@ -7,12 +7,17 @@ const Business = require('../models/Business');
 
 function createBusiness(req, res){
   client.business(req.params.businessId).then(response => {
-    Business.create({
-      yelpID: response.jsonBody.id,
-      name: response.jsonBody.name,
-      address: response.jsonBody.name,
-      img_url: response.jsonBody.image_url
-    })
+    if (!Business.find({yelpID:req.params.businessId})){
+      Business.create({
+        yelpID: response.jsonBody.id,
+        name: response.jsonBody.name,
+        address: response.jsonBody.name,
+        img_url: response.jsonBody.image_url
+      })
+    } else {
+      res.json({message: "The business already exists"})
+    }
+
 })
 }
 
