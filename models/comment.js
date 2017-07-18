c9ac7d5c08b8281fc4c8f910963e5ff81db62b95
Johnathon Sykes <hireme@mrjsykes.com>
2017-07-18 10:58:02 -0700

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const commentSchema = new mongoose.Schema({
   title: {type: String, required: true},
   body: {type: String, required: true},
-  _by: {type:mongoose.Schema.Types.ObjectId, ref: 'userSchema'}
+  _by: {type:mongoose.Schema.Types.ObjectId, ref: 'User'}
 })
 
 module.exports = mongoose.model('Comment', commentSchema)
